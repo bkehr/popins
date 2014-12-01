b0bc3b640ba2173e24c5c618ce589fd5a6b66c7e
Birte Kehr <birte.kehr@decode.is>
2014-12-01 18:17:40 +0000

    Renaming variables for readability.

--- popins_merge.h
+++ popins_merge.h
@@ -86,7 +86,7 @@ typename ComponentGraph<TSeq1>::TVertexDescriptor
 addVertex(ComponentGraph<TSeq1> & graph, TSeq2 & seq)
 {
     typename ComponentGraph<TSeq1>::TVertexDescriptor v = addVertex(graph.graph);
-    if (v != length(graph.sequenceMap)) std::cerr << "WARNING: The vertex map is broken.";
+    SEQAN_ASSERT_NEQ(v, length(graph.sequenceMap));
     appendValue(graph.sequenceMap, seq);
     return v;
 }
@@ -108,12 +108,12 @@ splitVertex(ComponentGraph<TSeq> & graph,
     {
         addEdge(graph.graph, v, targetVertex(it));
     }
-    
+
     removeOutEdges(graph.graph, u);
     graph.sequenceMap[u] = uSeq;
-    
+
     addEdge(graph.graph, u, v);
-    
+
     return v;
 }
 
@@ -548,8 +548,8 @@ template<typename TSeq1, typename TSeq2, typename TVertexDescriptor, typename TL
 bool mergeSeqWithGraph(ComponentGraph<TSeq1> & compGraph,
                        Path<TSeq1, TVertexDescriptor> & path,
                        TSeq2 & seq,
-                       Gaps<TSeq1> & gapsH,
-                       Gaps<TSeq2> & gapsV,
+                       Gaps<TSeq1> & gapsPath,
+                       Gaps<TSeq2> & gapsSeq,
                        TLength minBranchLen)
 {
     typedef typename Position<TSeq1>::Type TPos;
@@ -557,29 +557,28 @@ bool mergeSeqWithGraph(ComponentGraph<TSeq1> & compGraph,
 
     // --- handle right end of alignment
 
-    TPos endPosV = toSourcePosition(gapsV, length(gapsV)); // end position of alignment in seq
-    TPos endPosH = toSourcePosition(gapsH, length(gapsH)); // end position of alignment in path.seq
+    TPos alignEndSeq = toSourcePosition(gapsSeq, length(gapsSeq)); // end position of alignment in seq
+    TPos alignEndPath = toSourcePosition(gapsPath, length(gapsPath)); // end position of alignment in path.seq
 
-    if (endPosV < length(seq))
+    if (alignEndSeq < length(seq))
     {
-        TPos vPos = path.positionMap.lower_bound(endPosH)->first;
+        TPos vPos = path.positionMap.lower_bound(alignEndPath)->first; // end position of vertex label on path
         TVertexDescriptor v = path.positionMap[vPos];
-            
-        if (endPosH == length(path.seq))
-        {
-            append(compGraph.sequenceMap[v], suffix(seq, endPosV));
-        }
-        else if (length(seq) - endPosV > (TSize)minBranchLen)
-        {
 
-            if (vPos > endPosH)
+        if (alignEndPath == length(path.seq)) // alignment ends at end of path
+        {
+            append(compGraph.sequenceMap[v], suffix(seq, alignEndSeq));
+        }
+        else if (length(seq) - alignEndSeq > (TSize)minBranchLen) // unaligned part of seq is longer than minBranchLen
+        {
+            if (vPos > alignEndPath) // alignment ends before end of vertex label
             {
-                TPos splitPos = length(compGraph.sequenceMap[v]) - (vPos - endPosH);
+                TPos splitPos = length(compGraph.sequenceMap[v]) - (vPos - alignEndPath); // relative to vertex label
                 TSeq1 prefixSeq = prefix(compGraph.sequenceMap[v], splitPos);
                 TSeq1 suffixSeq = suffix(compGraph.sequenceMap[v], splitPos);
                 splitVertex(compGraph, v, prefixSeq, suffixSeq);
             }
-            TSeq1 suf = suffix(seq, endPosV);
+            TSeq1 suf = suffix(seq, alignEndSeq);
             TVertexDescriptor vBranch = addVertex(compGraph, suf);
             addEdge(compGraph.graph, v, vBranch);
         }
@@ -587,30 +586,29 @@ bool mergeSeqWithGraph(ComponentGraph<TSeq1> & compGraph,
 
     // --- handle left end of alignment
 
-    TPos beginPosV = toSourcePosition(gapsV, 0); // begin position of alignment in seq
-    TPos beginPosH = toSourcePosition(gapsH, 0); // begin position of alignment in path.seq
+    TPos alignBeginSeq = toSourcePosition(gapsSeq, 0); // begin position of alignment in seq
+    TPos alignBeginPath = toSourcePosition(gapsPath, 0); // begin position of alignment in path.seq
 
-    if (beginPosV > 0)
+    if (alignBeginSeq > 0)
     {
-        TPos uPos = path.positionMap.lower_bound(beginPosH)->first;
+        TPos uPos = path.positionMap.lower_bound(alignBeginPath)->first;
         TVertexDescriptor u = path.positionMap[uPos];
         
-        if (beginPosH == 0)
+        if (alignBeginPath == 0)
         {
-            replace(compGraph.sequenceMap[u], 0, 0, prefix(seq, beginPosV));
+            replace(compGraph.sequenceMap[u], 0, 0, prefix(seq, alignBeginSeq));
         }
-        else if (beginPosV > (TSize)minBranchLen)
+        else if (alignBeginSeq > (TSize)minBranchLen)
         {
-    
             TVertexDescriptor uSplit = u;
-            if (uPos - length(compGraph.sequenceMap[u]) < beginPosH)
+            if (uPos - length(compGraph.sequenceMap[u]) < alignBeginPath)
             {
-                TPos splitPos = length(compGraph.sequenceMap[u]) - (uPos - beginPosH);
+                TPos splitPos = length(compGraph.sequenceMap[u]) - (uPos - alignBeginPath);
                 TSeq1 prefixSeq = prefix(compGraph.sequenceMap[u], splitPos);
                 TSeq1 suffixSeq = suffix(compGraph.sequenceMap[u], splitPos);
                 uSplit = splitVertex(compGraph, u, prefixSeq, suffixSeq);
             }
-            TSeq1 pref = prefix(seq, beginPosV);
+            TSeq1 pref = prefix(seq, alignBeginSeq);
             TVertexDescriptor uBranch = addVertex(compGraph, pref);
             appendValue(compGraph.sources, uBranch);
             addEdge(compGraph.graph, uBranch, uSplit);
@@ -647,29 +645,30 @@ addSequencesToGraph(ComponentGraph<TSeq1> & compGraph,
 
         TScoreValue maxScore = minValue<TScoreValue>();
         TPath bestPath;
-        Gaps<TSeq1> bestH;
-        Gaps<TSeq2> bestV;
+        Gaps<TSeq1> bestGapsPath;
+        Gaps<TSeq2> bestGapsSeq;
 
         for (TSize j = 0; j < length(paths); ++j)
         {
-            Gaps<TSeq1> gapsH(paths[j].seq);
-            Gaps<TSeq2> gapsV(seqs[i]);
+            Gaps<TSeq1> gapsPath(paths[j].seq);
+            Gaps<TSeq2> gapsSeq(seqs[i]);
 
             int diag = bestDiagonal(seqs[i], paths[j].seq, qgramLength);
 
             TScoreValue val = 0;
-            if (diag == maxValue<int>()) val = localAlignment(gapsH, gapsV, scoringScheme);
-            else val = localAlignment(gapsH, gapsV, scoringScheme, diag-25, diag+25);
+            if (diag == maxValue<int>()) val = localAlignment(gapsPath, gapsSeq, scoringScheme);
+            else val = localAlignment(gapsPath, gapsSeq, scoringScheme, diag-25, diag+25);
+
             if (val > maxScore)
             {
                 maxScore = val;
                 bestPath = paths[j];
-                bestH = gapsH;
-                bestV = gapsV;
+                bestGapsPath = gapsPath;
+                bestGapsSeq = gapsSeq;
             }
         }
 
-        mergeSeqWithGraph(compGraph, bestPath, seqs[i], bestH, bestV, minBranchLen);
+        mergeSeqWithGraph(compGraph, bestPath, seqs[i], bestGapsPath, bestGapsSeq, minBranchLen);
     }
 
     return true;
@@ -696,10 +695,10 @@ mergeSequences(String<TSeq1> & mergedSeqs,
 
     TGraph compGraph(seqs[0]);
     bool ret = addSequencesToGraph(compGraph, seqs, minBranchLen, matchScore, errorPenalty, qgramLength);
-    
+
     String<TPath> finalPaths;
     enumeratePaths(finalPaths, compGraph);
-    
+
     if (verbose && numVertices(compGraph.graph) > 1)
     {
         std::cout << compGraph.graph;
@@ -712,7 +711,7 @@ mergeSequences(String<TSeq1> & mergedSeqs,
 
     for (TSize i = 0; i < length(finalPaths); ++i)
         appendValue(mergedSeqs, finalPaths[i].seq);
-        
+
     return ret;
 }
 
